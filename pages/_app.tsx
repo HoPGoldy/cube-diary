e1fbc897c47f9f7309aab400e84abc2ea6d77fb6
hopgoldy <hpgless@outlook.com>
2022-04-10 17:48:02 +0800

    feat: vconsole 手机调试

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ConfigProvider, Loading } from 'react-vant'
+import { ConfigProvider } from 'react-vant'
 import Head from 'next/head'
 import { useAppConfig, useUserProfile } from 'services/user'
 import { createContext, Dispatch, SetStateAction } from 'react'
 import { FontendConfig } from 'types/global'
 import { PageLoading } from 'components/PageLoading'
 import { UserProfile } from 'types/storage'
+// import Script from 'next/script'
 
 const themeVars = {
     passwordInputBackgroundColor: '#f7f8fa'
@@ -27,6 +28,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     return (
         <ConfigProvider themeVars={themeVars}>
+            {/* <Script src="https://cdn.bootcss.com/vConsole/3.2.0/vconsole.min.js" strategy="lazyOnload" onLoad={() => {
+                new VConsole()
+            }} /> */}
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
