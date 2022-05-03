4adbb6191e28f3390b6dff1a0e5a4f34b91b80ff
hopgoldy <hpgless@outlook.com>
2022-05-03 18:30:36 +0800

    fix: 移除 app.tsx 无用代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,12 @@
 import '../styles/globals.css'
-import type { AppContext, AppProps } from 'next/app'
-import { ConfigProvider } from 'react-vant'
+import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useAppConfig, useUserProfile } from 'services/user'
 import { createContext, Dispatch, SetStateAction, useEffect } from 'react'
 import { FontendConfig } from 'types/global'
 import { PageLoading } from 'components/PageLoading'
 import { UserProfile } from 'types/storage'
-import App from "next/app"
-import nookies from 'nookies'
-import { USER_TOKEN_KEY } from 'lib/constants'
-import { verifyAuth } from 'lib/auth'
-// import Script from 'next/script'
-
-const lightTheme = {
-    // passwordInputBackgroundColor: '#ffffff',
-    // diaryMainColor: '#000'
-}
-
-const darkTheme = {
-    // 基础配色
-    // diaryMainColor: '#9CA3AF',
-    // diaryActiveColor: '#3C4A60',
-    // diaryCardBackgroundColor: '#1E293B',
-    // diaryBackgroundColor: '#0F172A',
-    // // 前景色
-    // pickerOptionTextColor: "var(--rv-diary-main-color)",
-    // cardColor: 'var(--rv-diary-main-color)',
-    // cardHeaderColor: 'var(--rv-diary-main-color)',
-    // cellTextColor: 'var(--rv-diary-main-color)',
-    // fieldInputTextColor: "var(--rv-diary-main-color)",
-    // radioLabelColor: 'var(--rv-diary-main-color)',
-    // buttonDefaultColor: 'var(--rv-diary-main-color)',
-    // // 背景色
-    // pickerBackgroundColor: "var(--rv-diary-card-background-color)",
-    // backgroundColor: 'var(--rv-diary-background-color)',
-    // cardBackgroundColor: 'var(--rv-diary-card-background-color)',
-    // cellBackgroundColor: 'var(--rv-diary-card-background-color)',
-    // popupBackgroundColor: 'var(--rv-diary-card-background-color)',
-    // cellActiveColor: 'var(--rv-diary-active-color)',
-    // dialogBackgroundColor: 'var(--rv-diary-active-color)',
-    // buttonDefaultBackgroundColor: 'var(--rv-diary-active-color)',
-}
-
+import Script from 'next/script'
 export const UserConfigContext = createContext<FontendConfig | false | undefined>(undefined)
 
 interface IUserProfileContext {
@@ -61,19 +25,19 @@ function MyApp({ Component, pageProps }: AppProps) {
     }, [userInfo.userProfile?.darkTheme])
 
     return (
-        <ConfigProvider themeVars={userInfo.userProfile?.darkTheme ? darkTheme : lightTheme}>
-            {/* <Script src="https://cdn.bootcss.com/vConsole/3.2.0/vconsole.min.js" strategy="lazyOnload" onLoad={() => {
-                new VConsole()
-            }} /> */}
+        <>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            {/* <Script src="https://cdn.bootcss.com/vConsole/3.2.0/vconsole.min.js" strategy="lazyOnload" onLoad={() => {
+                new VConsole()
+            }} /> */}
             <UserProfileContext.Provider value={userInfo}>
                 <UserConfigContext.Provider value={config}>
                     {config !== undefined ? <Component {...pageProps} /> : <PageLoading />}
                 </UserConfigContext.Provider>
             </UserProfileContext.Provider>
-        </ConfigProvider>
+        </>
     )
 }
 
