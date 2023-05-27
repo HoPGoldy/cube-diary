a7bed3c6c4e80e2c80bd996761698e6ad81155ad
hopgoldy <hpgless@outlook.com>
2023-05-27 22:15:58 +0800

    feat: 文件名优化

--- src/client/utils/input.ts
+++
@@ -1,6 +0,0 @@
-import { KeyboardEvent } from 'react'
-
-export const blurOnEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== 'Enter') return
-    (e.target as HTMLInputElement).blur()
-}
